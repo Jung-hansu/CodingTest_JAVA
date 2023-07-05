da5cf5089acf6786bf9bfe1e3fe870c59476ac76
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-07-05 20:24:42 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.78 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
 	public String solution(String[] participant, String[] completion) {
-		HashMap<String, Integer> map = new HashMap<>();
+		Map<String, Integer> map = new HashMap<>();
 		for (String p : participant)
 			map.put(p, map.getOrDefault(p, 0) + 1);
 		for (String c : completion) {
