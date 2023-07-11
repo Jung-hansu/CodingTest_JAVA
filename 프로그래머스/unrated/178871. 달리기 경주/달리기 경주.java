c3e3bcb8a7a677b75a5038d5b2ff2c7918207903
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-07-11 15:38:22 +0900

    [unrated] Title: 달리기 경주, Time: 0.08 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings){
+        Map<String, Integer> rankMap = new HashMap<>();
+        Map<Integer, String> playerMap = new HashMap<>();
+        String[] answer = new String[players.length];
+        for (int i = 0; i < players.length; i++){
+            rankMap.put(players[i], i);
+            playerMap.put(i, players[i]);
+        }
+        for (String calling : callings){
+            int rank = rankMap.get(calling);
+            rankMap.put(calling, rank-1);
+            rankMap.put(playerMap.get(rank-1), rank);
+            playerMap.put(rank, playerMap.get(rank-1));
+            playerMap.put(rank-1, calling);
+        }
+        for (int i = 0; i < players.length; i++)
+            answer[i] = playerMap.get(i);
+        return answer;
+    }
+}
