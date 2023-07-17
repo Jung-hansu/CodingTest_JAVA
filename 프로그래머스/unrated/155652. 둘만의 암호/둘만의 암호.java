7c44fe53f7f418a9854b3053c8d193982340ac12
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-07-17 16:27:56 +0900

    [unrated] Title: 둘만의 암호, Time: 0.13 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 class Solution {
     public String solution(String s, String skip, int index) {
-        List<Character> alpha = new LinkedList<>();
-        String answer = "";
+        StringBuilder alpha = new StringBuilder("abcdefghijklmnopqrstuvwxyz");
+        StringBuilder answer = new StringBuilder();
         
-        for (char c = 'a'; c <= 'z'; c++) alpha.add(c);
-        for (char c : skip.toCharArray()) alpha.remove((Character)c);
+        for (char c : skip.toCharArray())
+            alpha.deleteCharAt(alpha.indexOf(String.valueOf(c)));
         for (char c : s.toCharArray())
-            answer += alpha.get((alpha.indexOf(c)+index)%alpha.size());
-        return answer;
+            answer.append(alpha.charAt((alpha.indexOf(String.valueOf(c))+index)%alpha.length()));
+        return answer.toString();
     }
 }
