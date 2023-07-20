1f86e943a9e777d5a67dfb66f6a815ca55b29950
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-07-21 02:23:56 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+	public int[] solution(int brown, int yellow) {
+		int h, total = brown + yellow;
+
+		for (h = 3; h * h <= total; h++)
+			if ((total/h - 2) * (h - 2) == yellow)
+				break;
+		return new int[]{total/h, h};
+	}
+}
