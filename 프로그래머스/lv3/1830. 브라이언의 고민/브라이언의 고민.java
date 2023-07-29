089a8d2e23d32eed454b8c6677cec1775f5209e5
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-07-29 18:38:13 +0900

    [level 3] Title: 브라이언의 고민, Time: 99.83 ms, Memory: 126 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/1830. 브라이언의 고민/브라이언의 고민.java
@@ -0,0 +1,104 @@
+import java.util.*;
+
+class Solution {
+	private Set<Character> used = new HashSet<>();
+	private StringBuilder sb;
+	private String answer = "";
+
+	public String solution(String sentence) {
+		sb = new StringBuilder(sentence);
+
+		//Check space
+		if (sb.indexOf(" ") >= 0)
+			return "invalid";
+
+		//Check rules
+		try{ checkRule(); }
+		catch(Exception e){ return "invalid"; }
+
+		//Check invalid parsing
+		for (char c : answer.toCharArray())
+			if (!(isUpper(c) || c == ' '))
+				return "invalid";
+
+		return answer.trim();
+	}
+
+	private void checkRule() throws Exception{
+		StringBuilder s;
+		char c;
+
+		while (!sb.toString().equals("")) {
+			s = parseWord();
+			System.out.println(s);
+			//Check rule 2
+			if (!isUpper(c = s.charAt(0))) {
+				if (used.contains(c) || s.charAt(s.length()-1) != c || s.length() == 2)
+					throw new Exception();
+				used.add(c);
+				s.deleteCharAt(0);
+				s.deleteCharAt(s.length() - 1);
+			}
+
+			//Check rule 1
+			if (s.length() > 1 && !isUpper(c = s.charAt(1))){
+				if (used.contains(c))
+					throw new Exception();
+				used.add(c);
+				for (int i = 1; i < s.length(); i++)
+					if (s.charAt(i) != c || !isUpper(s.deleteCharAt(i).charAt(i)))
+						throw new Exception();
+			}
+
+			answer += s.append(" ");
+		}
+	}
+
+	private StringBuilder parseWord(){
+		String ret;
+		int idx;
+		char c;
+
+		if (sb.length() == 1) {
+			ret = sb.substring(0, 1);
+			sb.delete(0,1);
+			return new StringBuilder(ret);
+		}
+		//Parse with rule 2
+		if (!isUpper(c = sb.charAt(0))){
+			for (idx = 1; idx < sb.length() && sb.charAt(idx) != c; idx++);
+			if (idx == sb.length())
+				return null;
+			ret = sb.substring(0, idx+1);
+			sb.delete(0, idx+1);
+			return new StringBuilder(ret);
+		}
+
+		//Parse with rule 1
+		if (!isUpper(c = sb.charAt(1))){
+			//When the word has only a character
+			int lowers = 1;
+			for (idx = 2; idx < sb.length(); idx++)
+				if (sb.charAt(idx) == c) lowers++;
+			if (lowers == 2){
+				ret = sb.substring(0, 1);
+				sb.delete(0, 1);
+				return new StringBuilder(ret);
+			}
+			//When the word has several characters
+			for (idx = 1; idx < sb.length() && sb.charAt(idx) == c; idx += 2);
+			ret = sb.substring(0, idx);
+			sb.delete(0, idx);
+			return new StringBuilder(ret);
+		}
+
+		//Parse with no rule
+		ret = sb.substring(0, 1);
+		sb.delete(0, 1);
+		return new StringBuilder(ret);
+	}
+
+	private boolean isUpper(char c){
+		return c >= 'A' && c <= 'Z';
+	}
+}
