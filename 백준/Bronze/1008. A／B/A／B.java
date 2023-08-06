1a56ed58bcb77b94d837c51a9b8bc2bc7de0dfe7
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-08-06 16:14:57 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		System.out.println(Double.parseDouble(st.nextToken())/Double.parseDouble(st.nextToken()));
+	}
+}
