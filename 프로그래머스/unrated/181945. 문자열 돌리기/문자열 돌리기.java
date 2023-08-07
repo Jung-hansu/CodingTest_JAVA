ff80f1d8bdd949369130e63c3819ead006f6ec03
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-08-08 00:44:01 +0900

    [unrated] Title: 문자열 돌리기, Time: 172.29 ms, Memory: 66.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (char c : br.readLine().toCharArray())
+            System.out.println(c);
+    }
+}
