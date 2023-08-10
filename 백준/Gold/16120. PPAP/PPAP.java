7b45b8da980be0354483102a8e6f758307b0ba64
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-08-10 19:11:42 +0900

    [Gold IV] Title: PPAP, Time: 280 ms, Memory: 20000 KB -BaekjoonHub

---
+++ 백준/Gold/16120. PPAP/PPAP.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    private static boolean isPPAP(String s){
+        int P = 1;
+
+        if (s.charAt(0) != 'P' || s.charAt(s.length()-1) != 'P') return false;
+        for (int i = 1; P > 0 && i < s.length(); i++)
+            if (s.charAt(i) == 'P') P++;
+            else if (s.charAt(++i) == 'P') P--;
+            else return false;
+        return P == 1;
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        if (isPPAP(br.readLine())) System.out.println("PPAP");
+        else System.out.println("NP");
+    }
+}
