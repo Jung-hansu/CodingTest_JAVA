b9c8ace7e4260178c5d8a9349e0c3935d5e63a90
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-08-10 15:34:22 +0900

    [level 3] Title: 정수 삼각형, Time: 8.10 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,8 +2,6 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0;
-
         for (int i = 1; i < triangle.length; i++){
             triangle[i][0] += triangle[i-1][0];
             triangle[i][triangle[i].length-1] += triangle[i-1][triangle[i-1].length-1];
