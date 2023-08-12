121e46870fb40f5ee82c296fc42ea44b114dca82
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-08-13 05:31:37 +0900

    [Bronze III] Title: 최소, 최대, Time: 488 ms, Memory: 88212 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -4,9 +4,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        br.readLine();
-        String s = br.readLine();
-        System.out.print(Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).min().getAsInt()+" ");
-        System.out.print(Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).max().getAsInt());
+        int N = Integer.parseInt(br.readLine()), min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while (N-- > 0){
+            int n = Integer.parseInt(st.nextToken());
+            min = Math.min(min, n);
+            max = Math.max(max, n);
+        }
+        System.out.println(min+" "+max);
     }
 }
