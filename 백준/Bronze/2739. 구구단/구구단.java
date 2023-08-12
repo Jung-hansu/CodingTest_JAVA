8cf34cb8513f9f7278daf3c0893a2cc389bf8ea3
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-08-13 04:10:49 +0900

    [Bronze V] Title: 구구단, Time: 156 ms, Memory: 16172 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -3,10 +3,8 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i < 10; i++) bw.write(N + " * " + i + " = " + (N*i) + "\n");
-        bw.flush();
+        for (int i = 1; i < 10; i++) System.out.println(N + " * " + i + " = " + (N*i));
     }
 }
