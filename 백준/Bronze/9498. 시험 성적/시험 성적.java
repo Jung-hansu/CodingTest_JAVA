355e1f94c017291978d5dfa17a5d868041fe9a9e
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-08-13 05:39:47 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N >= 90) System.out.println("A");
+        else if (N >= 80) System.out.println("B");
+        else if (N >= 70) System.out.println("C");
+        else if (N >= 60) System.out.println("D");
+        else System.out.println("F");
+    }
+}
