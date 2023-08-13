10e93d80794bde4c568f0f81b9b4c6154226457a
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-08-14 06:13:20 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 152 ms, Memory: 16100 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -2,14 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static int gcd(int n1, int n2){
-       return n1%n2 == 0 ? n2 : gcd(n2, n1%n2);
-    }
     public static void main(String[] args) throws IOException{
         StringTokenizer st = new StringTokenizer(new BufferedReader(new InputStreamReader(System.in)).readLine());
-        int A = Integer.parseInt(st.nextToken()), B = Integer.parseInt(st.nextToken()),
-            gcd = gcd(Math.max(A, B), Math.min(A, B));
+        int gcd, A = Integer.parseInt(st.nextToken()), B = Integer.parseInt(st.nextToken());
+        int[] AB = new int[]{Math.max(A,B), Math.min(A,B)};
 
+        for (int i = 0; AB[i%2] % (gcd = AB[(i+1)%2]) > 0; i++)
+            AB[i%2] %= AB[(i+1)%2];
         System.out.println(gcd+"\n"+(A*B/gcd));
     }
 }
