ac0e6f4f1f5ce49073b3503887255107237ce4c9
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-08-15 22:13:08 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 184 ms, Memory: 16412 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()), i;
+
+        for (i = 0; i < N; i++) System.out.println("*".repeat(i+1) + " ".repeat(2*(N-i-1)) + "*".repeat(i+1));
+        i--;
+        while (i-- > 0) System.out.println("*".repeat(i+1) + " ".repeat(2*(N-i-1)) + "*".repeat(i+1));
+    }
+}
