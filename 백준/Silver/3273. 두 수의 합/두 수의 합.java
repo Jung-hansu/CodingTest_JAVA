4953f28a6b2e9eab6a6f20e6217aa7c973168b80
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-08-20 07:48:17 +0900

    [Silver III] Title: 두 수의 합, Time: 356 ms, Memory: 26400 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -5,12 +5,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int l = 0, r = Integer.parseInt(br.readLine()) - 1, ans = 0;
-        int[] arr = Arrays.stream(br.readLine().split(" "))
-                          .mapToInt(Integer::parseInt)
-                          .sorted()
-                          .toArray();
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int x = Integer.parseInt(br.readLine());
-        while (l < r){
+        int[] arr = new int[r+1];
+
+        for (int i = 0; i < arr.length; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        while (l < r) {
             if (arr[l] + arr[r] == x) ans++;
             if (arr[l] + arr[r] <= x) l++;
             else if (arr[l] + arr[r] > x) r--;
