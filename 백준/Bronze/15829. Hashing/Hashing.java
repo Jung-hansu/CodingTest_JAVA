5f32f57272136218cc7adfed323bd4e8821055a8
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-08-20 20:35:49 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        long hash = 0;
+        String s = br.readLine();
+
+        for (int i = 0; i < L; i++)
+            hash = (hash + (s.charAt(i)-'a'+1)*(long)Math.pow(31, i)) % 1234567891;
+        System.out.println(hash);
+    }
+}
