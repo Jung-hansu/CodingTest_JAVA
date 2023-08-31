d49922f3fe3d47c41d138d197da6f0039276e928
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-08-31 18:17:19 +0900

    [Gold I] Title: 수들의 합 7, Time: 1640 ms, Memory: 329724 KB -BaekjoonHub

---
+++ 백준/Gold/2268. 수들의 합 7/수들의 합 7.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static class SegmentTree {
+        private long[] tree;
+        public SegmentTree(long[] arr) {
+            int tmp = Integer.highestOneBit(arr.length);
+            tree = new long[tmp << (tmp == arr.length ? 1 : 2)];
+            init(arr, 1, 0, arr.length - 1);
+        }
+
+        public long init(long[] arr, int node, int start, int end) {
+            if (start == end) return tree[node] = arr[start];
+            return tree[node] = init(arr, 2 * node, start, (start + end) / 2) +
+                    init(arr, 2 * node + 1, (start + end) / 2 + 1, end);
+        }
+        //원하는 값으로 변경
+        public void update(long[] arr, int idx, long val){
+            set(1, 0, arr.length-1, idx, val-arr[idx]);
+            arr[idx] = val;
+        }
+        private void set(int node, int start, int end, int idx, long diff) {
+            if (idx < start || idx > end) return;
+            tree[node] += diff;
+            if (start != end) {
+                set(2 * node, start, (start + end) / 2, idx, diff);
+                set(2 * node + 1, (start + end) / 2 + 1, end, idx, diff);
+            }
+        }
+        public long sum(int node, int start, int end, int from, int to) {
+            if (from > end || to < start) return 0;
+            if (from <= start && to >= end) return tree[node];
+            return sum(2 * node, start, (start + end) / 2, from, to) +
+                    sum(2 * node + 1, (start + end) / 2 + 1, end, from, to);
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()),
+            M = Integer.parseInt(st.nextToken());
+        long[] arr = new long[N];
+
+        SegmentTree segTree = new SegmentTree(arr);
+        for (int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()),
+                b = Integer.parseInt(st.nextToken()),
+                c = Integer.parseInt(st.nextToken());
+            if (a == 1) segTree.update(arr, b-1, c);
+            else bw.write(segTree.sum(1, 0, N-1, Math.min(b,c)-1, Math.max(b,c)-1)+"\n");
+        }
+        bw.close();
+    }
+}
