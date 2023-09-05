1fa381ac020e0e4c49e247a396d6835d163d48fe
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-09-05 14:04:20 +0900

    [Unrated] Title: 별 찍기 - 14, Time: 132 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Unrated/2556. 별 찍기 － 14/별 찍기 － 14.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) bw.write("*".repeat(N)+"\n");
+        bw.close();
+    }
+}
