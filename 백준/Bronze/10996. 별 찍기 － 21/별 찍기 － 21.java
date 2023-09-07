73035de670c8911a417f727f72d76f93c1b04a32
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-09-07 14:07:55 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 164 ms, Memory: 16488 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++)
+            bw.write("* ".repeat(N/2+N%2)+(N>1?"\n ":"")+"* ".repeat(N/2)+"\n");
+        bw.close();
+    }
+}
