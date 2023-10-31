bb84c10c87f344ad55c39322705eb1747fa551b3
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2023-10-31 14:40:19 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 116 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
