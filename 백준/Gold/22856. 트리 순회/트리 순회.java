a740735f9e89eee10f0624754a233614571dfc15
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2024-01-27 02:59:07 +0900

    [Gold IV] Title: 트리 순회, Time: 392 ms, Memory: 42408 KB -BaekjoonHub

--- 백준/Gold/22856. 트리 순회/트리 순회.java
+++ 백준/Gold/22856. 트리 순회/트리 순회.java
@@ -1,1 +1,20 @@
-import java.util.*;public class Main{public static void main(String[]args){Scanner s=new Scanner(System.in);int N=s.nextInt(),a=2*N-2,p;int[]r=new int[N+1];while(N-->0){p=s.nextInt();s.next();r[p]=s.nextInt();}for(int n=1;r[n]>0;n=r[n])a--;System.out.println(a);}}
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()), res = 2 * (N - 1);
+        int[] rightC = new int[N + 1];
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int p = Integer.parseInt(st.nextToken()),
+                l = Integer.parseInt(st.nextToken()),
+                r = Integer.parseInt(st.nextToken());
+            rightC[p] = r;
+        }
+        for (int n = 1; rightC[n] > 0; n = rightC[n]) res--;
+        System.out.println(res);
+    }
+}
