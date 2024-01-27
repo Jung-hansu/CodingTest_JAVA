08daf821ded8dcf125fa06cd39a584a5ae4c12c2
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2024-01-27 12:13:21 +0900

    [D3] Title: 암호문3, Time: 191 ms, Memory: 33,152 KB -BaekjoonHub

--- SWEA/D3/1230. 암호문3/암호문3.java
+++ SWEA/D3/1230. 암호문3/암호문3.java
@@ -1,83 +1,16 @@
 import java.io.*;
 import java.util.*;
 
-class MyLinkedList<T> implements Iterable<T>{
-    private int size;
-    private Node head = new Node(), rear = head;
-
-    private class Node {
-        T val;
-        Node next;
-
-        Node() {}
-        Node(T val) {this.val = val;}
-    }
-
-    public void add(T val) {
-        rear = rear.next = new Node(val);
-        size++;
-    }
-
-    public void add(T val, int idx) {
-        if (idx < 0 || idx > size)
-            throw new IndexOutOfBoundsException();
-        if (idx == size) {
-            add(val);
-            return;
-        }
-
-        Node node = head;
-        while (idx-- > 0)
-            node = node.next;
-
-        Node tmp = node.next;
-        node.next = new Node(val);
-        node.next.next = tmp;
-        size++;
-    }
-
-    public void remove(int idx) {
-        if (idx < 0 || idx >= size)
-            throw new IndexOutOfBoundsException();
-
-        Node node = head;
-        while (idx-- > 1)
-            node = node.next;
-
-        node.next = node.next.next;
-        size--;
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            private Node node = head;
-
-            @Override
-            public boolean hasNext() {
-                return node.next != null;
-            }
-
-            @Override
-            public T next() {
-                return (node = node.next).val;
-            }
-        };
-    }
-
-}
-
 public class Solution {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        String s;
 
-        for (int tc = 1; (s = br.readLine()) != null; tc++) {
-            MyLinkedList<Integer> codes = new MyLinkedList<>();
+        for (int tc = 1; tc <= 10; tc++) {
+            List<Integer> codes = new LinkedList<>();
+            int N = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(s);
             int M = Integer.parseInt(br.readLine());
 
             for (int i = 0; i < N; i++)
@@ -93,7 +26,7 @@ public class Solution {
                         cnt = Integer.parseInt(st.nextToken());
 
                         for (int i = 0; i < cnt; i++)
-                            codes.add(Integer.parseInt(st.nextToken()), idx + i);
+                            codes.add(idx + i, Integer.parseInt(st.nextToken()));
                         break;
                     case "D":
                         idx = Integer.parseInt(st.nextToken());
