6bd836aa08bb9000c56b9bc2e5e4b0bca5401485
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2024-01-27 11:00:50 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 102 ms, Memory: 19,352 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -1,14 +1,14 @@
 import java.io.*;
- 
+
 public class Solution {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-         
+
         for (int tc = 1; tc <= T; tc++) {
             int res = 0, bitSet = 0, N = Integer.parseInt(br.readLine());
-             
+
             while (bitSet < 1023) {
                 res += N;
                 for (int n = res; n > 0; n /= 10)
