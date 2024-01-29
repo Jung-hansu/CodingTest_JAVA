e1c0d0ff9113f0a61cb8fb5088112bedb82fb548
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2024-01-29 13:59:11 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 136 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class Main {
+	
+	private static StringBuilder sb = new StringBuilder();
+	
+	private static void append(String s, int depth) {
+		for (int i = 0; i < depth; i++)
+			sb.append("____");
+		sb.append(s).append('\n');
+	}
+	
+	private static void buildResponse(int N, int depth) {
+		append("\"재귀함수가 뭔가요?\"", depth);
+		
+		if (depth == N) {
+			append("\"재귀함수는 자기 자신을 호출하는 함수라네\"", depth);
+			append("라고 답변하였지.", depth);
+			return;
+		}
+		append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.", depth);
+		append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.", depth); 
+		append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"", depth);
+		
+		buildResponse(N, depth + 1);
+		append("라고 답변하였지.", depth);
+	}
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.", 0);
+		buildResponse(N, 0);
+		System.out.println(sb);
+	}
+	
+}
