2a9a1ca849a935a2ba4733145e6eeeecd8295538
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2024-01-30 13:14:23 +0900

    [Silver III] Title: N과 M (1), Time: 260 ms, Memory: 22608 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -4,32 +4,35 @@ import java.util.*;
 public class Main {
 
 	private static StringBuilder sb = new StringBuilder();
-	private static Stack<Integer> stack = new Stack<>();
+	private static int[] arr;
+	private static int N, M;
 	
-	private static void permutation(boolean[] check, int N, int M) {
-		if (stack.size() == M) {
-			stack.forEach(n -> sb.append(n).append(' '));
+	private static void permutation(boolean[] check, int idx) {
+		if (idx == M) {
+			for (int n : arr) {
+				sb.append(n).append(' ');
+			}
 			sb.append('\n');
 			return;
 		}
 		
 		for (int i = 1; i <= N; i++) 
 			if (!check[i]){
-				stack.push(i);
+				arr[idx] = i;
 				check[i] = true;
-				permutation(check, N, M);
+				permutation(check, idx + 1);
 				check[i] = false;
-				stack.pop();
 			}
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken()),
-			M = Integer.parseInt(st.nextToken());
 		
-		permutation(new boolean[N + 1], N, M);
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[M];
+		permutation(new boolean[N + 1], 0);
 		System.out.println(sb);
 	}
 	
