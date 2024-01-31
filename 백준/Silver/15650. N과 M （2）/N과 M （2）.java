7c78f0462aee960ba37e7b8668e3a716d7a97c47
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2024-01-31 14:37:30 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,27 +1,36 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static Stack<Integer> stack = new Stack<>();
-    private static void dfs(int N, int M, int idx, int depth) throws IOException {
-        if (depth == M){
-            for (int n : stack) bw.write(n+" ");
-            bw.newLine();
-            return;
-        }
-        for (int i = idx+1; i <= N; i++) {
-            stack.push(i);
-            dfs(N, M, i, depth + 1);
-            stack.pop();
-        }
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
 
-        dfs(N, M, 0, 0);
-        bw.close();
-    }
+	private static StringBuilder sb = new StringBuilder();
+	private static int N, M;
+	private static int[] arr;
+	
+	private static void combination(int idx) {
+		if (idx == M + 1) {
+			for (int i = 1; i <= M; i++) {
+				sb.append(arr[i]).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for (int i = arr[idx - 1] + 1; i <= N; i++) {
+			arr[idx] = i;
+			combination(idx + 1);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = br.readLine();
+		
+		N = line.charAt(0) - '0';
+		M = line.charAt(2) - '0';
+		arr = new int[M + 1];
+		
+		combination(1);
+		System.out.println(sb);
+	}
+	
 }
