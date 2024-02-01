bec2997924c89961096b1d106d9b49215c24fbe1
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2024-02-01 15:22:43 +0900

    [D2] Title: 파리 퇴치, Time: 105 ms, Memory: 18,664 KB -BaekjoonHub

---
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+        
+        for (int tc = 1; tc <= T; tc++) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+    		int maxKill = 0;
+        	int[][] sumField = new int[N + 1][N + 1];
+        	int[][] killField = new int[N + 1][N + 1];
+        	
+        	for (int i = 1; i <= N; i++) {
+        		st = new StringTokenizer(br.readLine());
+        		
+        		for (int j = 1; j <= N; j++) {
+        			int I = Math.max(i - M, 0), J = Math.max(j - M, 0);
+        			
+        			sumField[i][j] = sumField[i - 1][j] + sumField[i][j - 1] - sumField[i - 1][j - 1] + Integer.parseInt(st.nextToken());
+        			killField[i][j] = sumField[i][j] - sumField[I][j] - sumField[i][J] + sumField[I][J];
+        			maxKill = Math.max(maxKill, killField[i][j]);
+        		}
+        	}
+        	sb.append('#').append(tc).append(' ').append(maxKill).append('\n');
+        }
+        System.out.println(sb);
+    }
+	
+}
