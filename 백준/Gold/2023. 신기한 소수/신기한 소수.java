0217fbb189a7ea41630ccf02402e4658871427e4
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2024-02-01 17:56:55 +0900

    [Gold V] Title: 신기한 소수, Time: 72 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -5,30 +5,42 @@ public class Main {
 	
 	private static int[] primeCandidates = {1, 3, 7, 9};
 	
-	private static void getStrangePrime(BufferedWriter bw, int N, String prime) throws IOException{
-		if (prime.length() == N) {
-			bw.write(prime);
-			bw.newLine();
+	//이걸로 해보기
+	private static boolean isPrime(int num) {
+		if (num == 2) return true;
+		if (num % 2 == 0) return false;
+		
+		for (int i = 3; i <= (int)Math.sqrt(num); i += 2) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private static void getStrangePrime(StringBuilder sb, int N, int prime, int depth) {
+		if (depth == N) {
+			sb.append(prime).append('\n');
 			return;
 		}
 		
 		for (int n : primeCandidates) {
-			if (new BigInteger(prime + n).isProbablePrime(5)) {
-				getStrangePrime(bw, N, prime + n);
+			if (isPrime(prime * 10 + n)) {
+				getStrangePrime(sb, N, prime * 10 + n, depth + 1);
 			}
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String[] startPrime = {"2", "3", "5", "7"};
+		StringBuilder sb = new StringBuilder();
+		int[] startPrime = {2, 3, 5, 7};
 		int N = br.read() - '0';
 		
-		for (String prime : startPrime) {
-			getStrangePrime(bw, N, prime);
+		for (int prime : startPrime) {
+			getStrangePrime(sb, N, prime, 1);
 		}
-		bw.close();
+		System.out.println(sb);
 	}
 	
 }
