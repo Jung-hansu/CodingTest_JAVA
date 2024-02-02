9f2d606e156bd2cd9675feccc862109c48617078
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2024-02-02 17:12:45 +0900

    [Gold IV] Title: 친구비, Time: 220 ms, Memory: 18216 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.java
+++ 백준/Gold/16562. 친구비/친구비.java
@@ -1,44 +1,63 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
+	
 	private static int[] parent;
-	private static int[] cost;
-
-	private static void union(int f1, int f2){
-		int x = find(f1), y = find(f2);
-
-		if (cost[x] > cost[y]) parent[x] = y;
-		else parent[y] = x;
+	
+	private static void union(int a, int b) {
+		int x = find(a), y = find(b);
+		parent[Math.max(x, y)] = Math.min(x, y);
 	}
-
-	private static int find(int f){
-		if (parent[f] == f) return f;
-		return find(parent[f]);
+	
+	private static int find(int x) {
+		if (parent[x] == x) return x;
+		return parent[x] = find(parent[x]);
 	}
-
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-		Set<Integer> leaders = new HashSet<>();
-        int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
-		int money = Integer.parseInt(st.nextToken()), minCost = 0;
-
-		cost = new int[N+1];
-		parent = new int[N+1];
-        st = new StringTokenizer(br.readLine());
-	    for (int i = 1; i <= N; i++) cost[i] = Integer.parseInt(st.nextToken());
-	    for (int i = 1; i <= N; i++) parent[i] = i;
-                                
-		//Unionizing
-		while (M-- > 0){
-			st = new StringTokenizer(br.readLine());
-			union(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Map<Integer, Integer> groupCosts = new HashMap<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken()),
+			K = Integer.parseInt(st.nextToken()), cost = 0;
+		int[] costs = new int[N + 1];
+		
+		//init parent
+		parent = new int[N + 1];
+		for (int i = 0; i < N; i++) {
+			parent[i] = i;
 		}
-		//Gathering the cheapest friend of the group
-		for (int f : parent) leaders.add(find(f));
-
-	    for (int f : leaders) minCost += cost[f];
-	    System.out.println(money >= minCost ? minCost : "Oh no");
-    }
+		
+		//input parsing
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			costs[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		//union (grouping)
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+			
+			union(a, b);
+		}
+		
+		//각 그룹의 최소 친구비 계산 
+		for (int i = 1; i <= N; i++) {
+			int group = find(i);
+			groupCosts.put(group, Math.min(costs[i], groupCosts.getOrDefault(group, Integer.MAX_VALUE)));
+		}
+		
+		//각 그룹 모두 친구로 만듦
+		for (int val : groupCosts.values()) {
+			if ((cost += val) > K) {
+				System.out.println("Oh no");
+				return;
+			}
+		}
+		System.out.println(cost);
+	}
+	
 }
