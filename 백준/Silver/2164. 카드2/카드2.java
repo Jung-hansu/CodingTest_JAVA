032026a695aa5edadd9adb8cd6efb935c06b8d9b
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2024-02-02 15:55:06 +0900

    [Silver IV] Title: 카드2, Time: 148 ms, Memory: 25860 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -2,16 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Queue<Integer> q = new LinkedList<>();
-        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= N; i++) q.add(i);
-        while (q.size() > 1){
-            q.remove();
-            q.add(q.remove());
-        }
-        System.out.println(q.remove());
-    }
+	public static void main(String[] args) throws IOException {
+		Queue<Integer> q = new ArrayDeque<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Integer[] nums = new Integer[N];
+		
+		for (int i = 0; i < N; i++) {
+			nums[i] = i + 1;
+		}
+		q.addAll(Arrays.asList(nums));
+		while (q.size() > 1) {
+			q.remove();
+			q.add(q.remove());
+		}
+		System.out.println(q.remove());
+	}
+	
 }
