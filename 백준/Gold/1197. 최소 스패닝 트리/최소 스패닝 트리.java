f8a4f3f266d0175930d6759378dd7d763b3e5192
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2024-02-04 20:41:41 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 768 ms, Memory: 53516 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,43 +1,57 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
+
     private static int[] parent;
-    private static class Edge{
-        int A, B, C;
-        Edge(int A, int B, int C){ this.A = A; this.B = B; this.C = C; }
-    }
-    private static void union(int a, int b){
-        int x = find(a), y = find(b);
+    private static int res = 0;
+
+    private static boolean union(int[] edge) {
+        int x = find(edge[0]), y = find(edge[1]);
+
+        if (x == y) {
+            return false;
+        }
+        res += edge[2];
         parent[Math.max(x, y)] = Math.min(x, y);
+        return true;
     }
-    private static int find(int x){
+
+    private static int find(int x) {
         if (parent[x] == x) return x;
         return parent[x] = find(parent[x]);
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparing(o->o.C));
-        int V = Integer.parseInt(st.nextToken()), E = Integer.parseInt(st.nextToken()), res = 0;
+        int V = Integer.parseInt(st.nextToken()), E = Integer.parseInt(st.nextToken()), cnt = 0;
+        int[][] edges =  new int[E][];
 
-        parent = new int[V+1];
-        for (int i = 0; i <= V; i++) parent[i] = i;
-        for (int i = 0; i < E; i++){
+        //init parent
+        parent = new int[V + 1];
+        for (int i = 1; i <= V; i++) {
+            parent[i] = i;
+        }
+
+        //parsing edges
+        for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken()),
                 B = Integer.parseInt(st.nextToken()),
                 C = Integer.parseInt(st.nextToken());
-            pq.add(new Edge(A, B, C));
+            edges[i] = new int[]{A, B, C};
         }
-        for (int i = 0; i < V-1; i++){
-            Edge e;
-            do
-                e = pq.remove();
-            while (find(e.A) == find(e.B));
-            union(e.A, e.B);
-            res += e.C;
+
+        Arrays.sort(edges, Comparator.comparingInt(o -> o[2]));
+        for (int[] edge : edges) {
+            if (union(edge)) {
+                if (++cnt == V - 1) {
+                    break;
+                }
+            }
         }
         System.out.println(res);
     }
+
 }
