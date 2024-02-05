73b9b0955ae499d115242e9feb70eedf45c42d4e
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2024-02-05 21:25:41 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 공통조상, Time: 138 ms, Memory: 29,100 KB -BaekjoonHub

---
+++ SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    private static int[] parent, subtreeSizes;
+
+    private static void union(int p, int c){
+        parent[c] = p;
+        updateSubtreeSize(c, subtreeSizes[c]);
+    }
+
+    /** 각 노드가 갖고 있는 subtree 크기 업데이트 */
+    private static void updateSubtreeSize(int c, int addedChildren){
+        while (c != parent[c]) {
+            subtreeSizes[parent[c]] += addedChildren;
+            c = parent[c];
+        }
+    }
+
+    /** 하나씩 거슬러 올라가며 공통 조상 탐색 */
+    private static int getCommonAncestor(int A, int B){
+        BitSet bs = new BitSet();
+
+        while (A > 1 || B > 1) {
+            if (A > 1 && bs.get(A)) return A;
+            bs.set(A);
+            if (B > 1 && bs.get(B)) return B;
+            bs.set(B);
+            A = parent[A];
+            B = parent[B];
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) throws IOException {
+   	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken()), E = Integer.parseInt(st.nextToken()),
+                A = Integer.parseInt(st.nextToken()), B = Integer.parseInt(st.nextToken());
+
+            //init
+            parent = new int[V + 1];
+            subtreeSizes = new int[V + 1];
+            Arrays.fill(subtreeSizes, 1);
+            for (int i = 0; i <= V; i++) parent[i] = i;
+
+            //parse data
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < E; i++)
+                union(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+
+            int res = getCommonAncestor(A, B);
+            sb.append('#').append(tc).append(' ').append(res).append(' ').append(subtreeSizes[res]).append('\n');
+        }
+    
+        System.out.println(sb);
+    }
+    
+}
