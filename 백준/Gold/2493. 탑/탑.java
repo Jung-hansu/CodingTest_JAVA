0f459e4bb6c65d29c556916ec9c133998b379e03
정한수 <97068614+Jung-hansu@users.noreply.github.com>
2024-02-06 12:21:07 +0900

    [Gold V] Title: 탑, Time: 544 ms, Memory: 108012 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		Stack<int[]> stack = new Stack<>();
+		Deque<int[]> dq = new ArrayDeque<>();
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] towers = new int[N];
@@ -18,13 +18,13 @@ public class Main {
 		
 		//run
 		for (int i = N - 1; i >= 0; i--) {
-			while (!stack.isEmpty() && stack.peek()[1] <= towers[i]) {
-				towers[stack.pop()[0]] = i + 1;
+			while (!dq.isEmpty() && dq.peekLast()[1] <= towers[i]) {
+				towers[dq.pollLast()[0]] = i + 1;
 			}
-			stack.push(new int[] {i, towers[i]});
+			dq.addLast(new int[] {i, towers[i]});
 		}
-		while (!stack.isEmpty()) {
-			towers[stack.pop()[0]] = 0;
+		while (!dq.isEmpty()) {
+			towers[dq.pollLast()[0]] = 0;
 		}
 		
 		//print
