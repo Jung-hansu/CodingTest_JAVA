ac0d0eae19f9dbc00e2ac315d68b3487a9f7d3ff
정한수 <97068614+Jung-hansu@users.noreply.github.com>
2024-02-06 09:39:54 +0900

    [Gold II] Title: 친구 네트워크, Time: 1284 ms, Memory: 74432 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -1,54 +1,62 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    private static Map<String, String> parent;
-    private static Map<String, Integer> setSize;
-    private static int union(String f1, String f2){
-        String x = find(f1), y = find(f2);
-        if (x.compareTo(y)>0) {
-            if (x.equals(y)) return setSize.get(y);
-            parent.put(x, y);
-            setSize.put(y, setSize.get(y)+setSize.remove(x));
-            return setSize.get(y);
-        }
-        else {
-            if (x.equals(y)) return setSize.get(x);
-            parent.put(y, x);
-            setSize.put(x, setSize.get(x)+setSize.remove(y));
-            return setSize.get(x);
-        }
-    }
-    private static String find(String f){
-        if (f.equals(parent.get(f))) return f;
-        parent.put(f, find(parent.get(f)));
-        return parent.get(f);
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
-
-        while (T-- > 0) {
-            int F = Integer.parseInt(br.readLine());
-
-            parent = new HashMap<>();
-            setSize = new HashMap<>();
-            for (int i = 0; i < F; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                String f1 = st.nextToken(), f2 = st.nextToken();
-
-                if (!parent.containsKey(f1)){
-                    parent.put(f1, f1);
-                    setSize.put(f1, 1);
-                }
-                if (!parent.containsKey(f2)){
-                    parent.put(f2, f2);
-                    setSize.put(f2, 1);
-                }
-                bw.write(union(f1, f2) + "\n");
-            }
-        }
-        bw.close();
-    }
+class Main {
+	
+	/** 해당 노드가 루트일 때의 네트워크 크기(subtree 노드 수) */
+	private static int[] networkSize;
+	private static int[] parent;
+	
+	/** Union 연산 후 합쳐진 네트워크 크기(tree 노드 수) 반환 */
+	private static int union(int a, int b) {
+		int x = find(a), y = find(b);
+		
+		if (x == y) {
+			return networkSize[x];
+		}
+		parent[Math.max(x, y)] = Math.min(x, y);
+		return networkSize[Math.min(x, y)] += networkSize[Math.max(x, y)];
+	}
+	
+	private static int find(int x) {
+		if (parent[x] == x) return x;
+		return parent[x] = find(parent[x]);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		int T = Integer.parseInt(br.readLine());
+		
+		while (T-- > 0) {
+			Map<String, Integer> map = new HashMap<>();
+			int F = Integer.parseInt(br.readLine()), cnt = 0;
+			
+			//initialize
+			parent = new int[2 * F + 1];
+			networkSize = new int[2 * F + 1];
+			for (int i = 0; i <= 2 * F; i++) {
+				networkSize[i] = 1;
+				parent[i] = i;
+			}
+			
+			//union-find
+			for (int i = 0; i < F; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				String f1 = st.nextToken(), f2 = st.nextToken();
+				
+				if (!map.containsKey(f1)) {
+					map.put(f1, cnt++);
+				}
+				if (!map.containsKey(f2)) {
+					map.put(f2, cnt++);
+				}
+				
+				sb.append(union(map.get(f1), map.get(f2))).append('\n');
+			}
+		}
+		
+		System.out.println(sb);
+	}
+	
 }
