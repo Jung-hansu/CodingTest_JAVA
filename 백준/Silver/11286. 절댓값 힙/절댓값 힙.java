2b7fea31ea13e3b7adc929fe14f7270ea10307ea
정한수 <97068614+Jung-hansu@users.noreply.github.com>
2024-02-07 13:54:36 +0900

    [Silver I] Title: 절댓값 힙, Time: 416 ms, Memory: 27676 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,19 +1,26 @@
 import java.io.*;
 import java.util.*;
+import static java.lang.Math.abs;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o1 + o2 == 0 ? o1 - o2 : Math.abs(o1) - Math.abs(o2));
-        int N = Integer.parseInt(br.readLine());
 
-        while (N-- > 0){
-            int n = Integer.parseInt(br.readLine());
-            if (n == 0 && !pq.isEmpty()) bw.write(pq.remove()+"\n");
-            else if (n == 0) bw.write("0\n");
-            else pq.add(n);
-        }
-        bw.close();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> abs(o1) == abs(o2) ? o1 - o2 : abs(o1) - abs(o2));
+		
+		while (N-- > 0) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if (num != 0) {
+				pq.add(num);
+			}
+			else {
+				sb.append(pq.isEmpty() ? 0 : pq.remove()).append('\n');
+			}
+		}
+		System.out.println(sb);
+	}
+	
 }
