3d44cdb137e397786f9b85a63d27a10c461f5152
정한수 <97068614+Jung-hansu@users.noreply.github.com>
2024-02-07 15:41:49 +0900

    [Silver V] Title: 색종이, Time: 84 ms, Memory: 11772 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BitSet paper = new BitSet();
+		int N = Integer.parseInt(br.readLine());
+		
+		while (N-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
+			
+			for (int i = 0; i < 10; i++) {
+				int sIdx = 100 * (x + i) + y;
+				paper.set(sIdx, sIdx + 10);
+			}
+		}
+		System.out.println(paper.cardinality());
+	}
+	
+}
