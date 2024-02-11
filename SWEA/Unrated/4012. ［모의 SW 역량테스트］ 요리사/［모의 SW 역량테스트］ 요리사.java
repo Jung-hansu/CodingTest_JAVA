89679b66ce301eeeedd3f5f65a8982061d613603
정한수 <97068614+Jung-hansu@users.noreply.github.com>
2024-02-11 18:53:36 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 215 ms, Memory: 22,244 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -11,20 +11,17 @@ public class Solution {
         if (depth >= N / 2) {
             int diff = 0;
 
-            for (int i = 0; i < N - 1; i++){
-                for (int j = i + 1; j < N; j++){
-                    if (comb.get(i) ^ comb.get(j)) {
-                        continue;
+            for (int i = 0; i < N - 1; i++) {
+                for (int j = i + 1; j < N; j++) {
+                    if (!comb.get(i) ^ comb.get(j)) {
+                        diff += (synergy[i][j] + synergy[j][i]) * (comb.get(i) ? 1 : -1);
                     }
-                    //i, j가 같은 집합에 있을 때만 차이 계산
-                    diff += (synergy[i][j] + synergy[j][i]) * (comb.get(i) ? 1 : -1);
                 }
             }
             minDiff = Math.min(minDiff, Math.abs(diff));
             return;
         }
 
-        //N가지 중 N/2가지를 고르는 조합 구하기
         for (int n = num; n <= N / 2 + depth; n++){
             comb.set(n);
             combine(depth + 1, n + 1);
@@ -32,27 +29,29 @@ public class Solution {
         }
     }
 
+    private static int readInt() throws IOException {
+        int c, n = 0;
+        while ((c = System.in.read()) > 32){
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+    
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
+        int T = readInt();
 
         for (int tc = 1; tc <= T; tc++){
-            //init
-            N = Integer.parseInt(br.readLine());
+            N = readInt();
             synergy = new int[N][N];
             minDiff = Integer.MAX_VALUE;
 
-            //parsing
             for (int i = 0; i < N; i++){
-                StringTokenizer st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++){
-                    synergy[i][j] = Integer.parseInt(st.nextToken());
+                    synergy[i][j] = readInt();
                 }
             }
 
-            //run
-            //0을 선택하지 않는 경우만 세서 중복 예방
             combine(0, 1);
             sb.append('#').append(tc).append(' ').append(minDiff).append('\n');
         }
