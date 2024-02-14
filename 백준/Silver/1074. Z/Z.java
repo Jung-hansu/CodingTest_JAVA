52934a72872598a97c878e1267ddce40f9d4eaf1
정한수 <97068614+Jung-hansu@users.noreply.github.com>
2024-02-14 13:18:18 +0900

    [Silver I] Title: Z, Time: 80 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -2,17 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static int recursive(int N, int r, int c, int res){
-        if (N == 0) return res;
-        int area = r/(1<<(N-1))*2+c/(1<<(N-1));
-        return recursive(N-1, r-(area/2)*(1<<(N-1)), c-(area%2)*(1<<(N-1)), res+area*(1<<(2*N-2)));
-    }
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()),
-            r = Integer.parseInt(st.nextToken()),
-            c = Integer.parseInt(st.nextToken());
-        System.out.println(recursive(N, r, c, 0));
-    }
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()), r = Integer.parseInt(st.nextToken()),
+			c = Integer.parseInt(st.nextToken()), res = 0;
+		
+		for (int n = 1 << N; n > 0; n /= 2) {
+			if (r >= n / 2) {
+				r -= n / 2;
+				res += n * n / 2;
+			}
+			if (c >= n / 2) {
+				c -= n / 2;
+				res += n * n / 4;
+			}
+		}
+		System.out.println(res);
+	}
+	
 }
