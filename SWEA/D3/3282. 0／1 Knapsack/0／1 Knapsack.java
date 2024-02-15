c58c9bb28c41fe723c753130597fc46cbc768c1e
정한수 <97068614+Jung-hansu@users.noreply.github.com>
2024-02-16 01:54:52 +0900

    [D3] Title: 0/1 Knapsack, Time: 137 ms, Memory: 24,184 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -1,12 +1,18 @@
 import java.io.*;
-import java.util.*;
 
 public class Solution {
+    
+    private static int readInt() throws IOException{
+        int c, n = 0;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
 
     private static int knapsack(int[] volumes, int[] values, int N, int K){
-        //{n, k}: 용량 k짜리 배낭에 1~n까지의 아이템들을 넣을 때의 최대 value
         int[][] dp = new int[N + 1][K + 1];
-        
+
         for (int n = 1; n <= N; n++){
             for (int k = 1; k <= K; k++){
                 if (k >= volumes[n]){
@@ -21,19 +27,16 @@ public class Solution {
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
+        int T = readInt();
 
         for (int tc = 1; tc <= T; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken());
+            int N = readInt(), K = readInt();
             int[] volumes = new int[N + 1], values = new int[N + 1];
 
             for (int i = 1; i <= N; i++){
-                st = new StringTokenizer(br.readLine());
-                volumes[i] = Integer.parseInt(st.nextToken());
-                values[i] = Integer.parseInt(st.nextToken());
+                volumes[i] = readInt();
+                values[i] = readInt();
             }
 
             sb.append('#').append(tc).append(' ').append(knapsack(volumes, values, N, K)).append('\n');
