ca308dd328d5ba6b556b19dea289f6a0fda36293
정한수 <97068614+Jung-hansu@users.noreply.github.com>
2024-02-20 22:11:31 +0900

    [Gold V] Title: 암호 만들기, Time: 80 ms, Memory: 11672 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -32,23 +32,35 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        L = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        L = readInt();
+        C = readInt();
         chars = new char[C];
         codes = new char[L];
 
-        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < C; i++){
-            chars[i] = st.nextToken().charAt(0);
+            chars[i] = readChar();
         }
 
         Arrays.sort(chars);
         dfs(sb, 0, 0, 0, 0);
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 
+    private static int readInt() throws IOException{
+        int c, n = 0;
+
+        while ((c = System.in.read()) > 32)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    private static char readChar() throws IOException{
+        int c;
+
+        do
+            c = System.in.read();
+        while(c <= 32);
+        return (char)c;
+    }
 }
