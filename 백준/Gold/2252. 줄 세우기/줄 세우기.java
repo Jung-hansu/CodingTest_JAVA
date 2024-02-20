6bb0174499156c173b218276fa53680185eabf48
정한수 <97068614+Jung-hansu@users.noreply.github.com>
2024-02-20 14:44:04 +0900

    [Gold III] Title: 줄 세우기, Time: 396 ms, Memory: 45608 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -2,33 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
-        List<Integer>[] nextNode = new List[N + 1];
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-        int[] inDegree = new int[N + 1];
-
-        for (int i = 1; i <= N; i++)nextNode[i] = new ArrayList<>();
-        for (int i = 0; i < M; i++){
-            st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken()), B = Integer.parseInt(st.nextToken());
-            nextNode[A].add(B);
-            inDegree[B]++;
-        }
-
-        for (int i = 1; i < inDegree.length; i++)
-            if (inDegree[i] == 0)
-                q.add(i);
-        while(!q.isEmpty()){
-            int tmp = q.remove();
-            sb.append(tmp).append(" ");
-            for (int next : nextNode[tmp])
-                if (--inDegree[next] == 0)
-                    q.add(next);
-        }
-        System.out.println(sb);
-    }
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+		int[] inDegrees = new int[N + 1];
+		
+		//init adj
+		List<Integer>[] adj = new List[N + 1];
+		for (int i = 1; i <= N; i++) {
+			adj[i] = new ArrayList<>();
+		}
+		
+		//parse adj
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			adj[a].add(b);
+			inDegrees[b]++;
+		}
+		
+		//선행 노드가 없는 시작 노드들 모두 큐에 삽입
+		Queue<Integer> q = new ArrayDeque<>();
+		for (int i = 1; i <= N; i++) {
+			if (inDegrees[i] == 0) {
+				q.add(i);
+			}
+		}
+		
+		//선행 노드들이 모두 사라지면 큐에 삽입
+		while(!q.isEmpty()) {
+			int tmp = q.remove();
+			
+			for (int v : adj[tmp]) {
+				if (--inDegrees[v] == 0) {
+					q.add(v);
+				}
+			}
+			sb.append(tmp).append(' ');
+		}
+		
+		System.out.println(sb);
+	}
+	
 }
