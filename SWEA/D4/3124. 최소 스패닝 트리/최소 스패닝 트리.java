69dcb3b07cc30fb36525ee86de76b59489463abd
정한수 <97068614+Jung-hansu@users.noreply.github.com>
2024-02-22 16:38:56 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,880 ms, Memory: 216,240 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,12 +3,13 @@ import java.util.*;
 
 public class Solution {
 	
+	private static List<Edge>[] adj;
+	
 	private static class Edge implements Comparable<Edge>{
-		int A, B, cost;
+		int to, cost;
 
-		public Edge(int a, int b, int cost) {
-			A = a;
-			B = b;
+		public Edge(int to, int cost) {
+			this.to = to;
 			this.cost = cost;
 		}
 		
@@ -17,43 +18,30 @@ public class Solution {
 			return this.cost - e.cost;
 		}
 	}
-	
-	private static int[] parent;
-	private static int[] size;
-	
-	private static boolean union(int a, int b) {
-		int x = find(a), y = find(b);
-		
-		if (x == y) {
-			return false;
-		}
-		
-		if (size[x] < size[y]) {
-			size[x] += size[y];
-			parent[x] = y;
-		} else {
-			size[y] += size[x];
-			parent[y] = x;
-		}
-		return true;
-	}
-	
-	private static int find(int x) {
-		if (parent[x] == x) return x;
-		return parent[x] = find(parent[x]);
-	}
-	
-	private static long kruskal(PriorityQueue<Edge> pq, int V) {
+
+	private static long prim(int V) {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		BitSet selected = new BitSet();
 		long res = 0;
 		
-		for (int i = 0; i < V - 1; i++) {
-			Edge e = pq.remove();
+		selected.set(1);
+		for (Edge e : adj[1]) {
+			pq.add(e);
+		}
+		
+		while(--V > 0) {
+			Edge edge;
+			do {
+				edge = pq.remove();
+			} while (selected.get(edge.to));
 			
-			if (!union(e.A, e.B)) {
-				i--;
-				continue;
+			res += edge.cost;
+			selected.set(edge.to);
+			for (Edge e : adj[edge.to]) {
+				if (!selected.get(e.to)) {
+					pq.add(e);
+				}
 			}
-			res += e.cost;
 		}
 		return res;
 	}
@@ -64,17 +52,14 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc <= T; tc++) {
-			PriorityQueue<Edge> pq = new PriorityQueue<>();
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int V = Integer.parseInt(st.nextToken());
 			int E = Integer.parseInt(st.nextToken());
 			
-			//initialize parent
-			parent = new int[V + 1];
-			size = new int[V + 1];
+			//initialize adjacency list
+			adj = new List[V + 1];
 			for (int i = 1; i <= V; i++) {
-				parent[i] = i;
-				size[i] = 1;
+				adj[i] = new ArrayList<>();
 			}
 			
 			//parse edges
@@ -83,12 +68,13 @@ public class Solution {
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
 				int cost = Integer.parseInt(st.nextToken());
-				
-				pq.add(new Edge(a, b, cost));
+			
+				adj[a].add(new Edge(b, cost));
+				adj[b].add(new Edge(a, cost));
 			}
 			
 			//run & print
-			sb.append('#').append(tc).append(' ').append(kruskal(pq, V)).append('\n');
+			sb.append('#').append(tc).append(' ').append(prim(V)).append('\n');
 		}
 		System.out.print(sb);
 	}
