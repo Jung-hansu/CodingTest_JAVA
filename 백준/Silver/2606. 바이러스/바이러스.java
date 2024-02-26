7c292e3fe545881c3c0e6fc887900f57ebf8ba4a
정한수 <97068614+Jung-hansu@users.noreply.github.com>
2024-02-26 16:03:44 +0900

    [Silver III] Title: 바이러스, Time: 84 ms, Memory: 11752 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,31 +1,49 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static int[] parent;
-    private static void union(int a, int b){
-        int x = find(a), y = find(b);
-        parent[Math.max(x, y)] = Math.min(x, y);
-    }
-    private static int find(int x){
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()), M = Integer.parseInt(br.readLine()), cnt = 0;
-
-        parent = new int[N + 1];
-        for (int i = 0; i <= N; i++) parent[i] = i;
-        while (M-- > 0){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
-            union(a, b);
-        }
-
-        for (int i = 2; i <= N; i++)
-            if (find(i) == 1)
-                cnt++;
-        System.out.println(cnt);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		List<Integer>[] edge = new List[N+1];
+		for(int i =1; i<=N; i++) {
+			edge[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			edge[a].add(b);
+			edge[b].add(a);
+		}
+		
+		boolean[] visited = new boolean[N+1];
+		visited[1] = true;
+		int answer = 0;
+		Deque<Integer> deque = new ArrayDeque<>();
+		deque.add(1);
+		while(!deque.isEmpty()) {
+			int curr = deque.removeFirst();
+			for (Integer num : edge[curr]) {
+				if(!visited[num]) {
+					visited[num] = true;
+					answer+=1;
+					deque.add(num);
+				}
+			}
+		}
+		System.out.println(answer);
+	}
 }
