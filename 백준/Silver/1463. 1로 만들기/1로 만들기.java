ad741187964ba4553a290c4846c794f366fd002b
정한수 <97068614+Jung-hansu@users.noreply.github.com>
2024-02-27 11:38:09 +0900

    [Silver III] Title: 1로 만들기, Time: 100 ms, Memory: 15532 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,33 +1,23 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    private static int bfs(int N){
-        Queue<Integer> q = new LinkedList<>();
-        int res = 0;
 
-        q.add(N);
-    a:  while(!q.isEmpty()){
-            res++;
-            for (int i = q.size(); i > 0; i--) {
-                int tmp = q.remove();
-
-                if (tmp % 3 == 0) {
-                    if (tmp / 3 == 1) break a;
-                    q.add(tmp/3);}
-                if (tmp % 2 == 0){
-                    if (tmp / 2 == 1) break a;
-                    q.add(tmp/2);}
-                if (tmp > 1) {
-                    if (tmp == 2) break a;
-                    q.add(tmp - 1);}
-            }}
-        return res;
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        System.out.println(N==1?0:bfs(N));
-    }
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+		int[] dp = new int[N + 1];
+		
+		for (int i = 2; i <= N ;i++) {
+			dp[i] = dp[i - 1] + 1;
+			
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+			}
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+			}
+		}
+		
+		System.out.println(dp[N]);
+	}
+	
 }
