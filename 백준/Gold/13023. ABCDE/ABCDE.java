7a96c98088e4e2a93f4de2a6947986c8227110dc
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-02-28 09:41:52 +0900

    [Gold V] Title: ABCDE, Time: 92 ms, Memory: 12044 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -36,10 +36,17 @@ public class Main {
         return visited[n] = false;
     }
 
+    private static int readInt() throws IOException {
+        int c, n = 0;
+        
+        while((c = System.in.read()) > 32){
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+    
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+        int N = readInt(), M = readInt();
 
         //init adj
         adj = new List[N];
@@ -51,9 +58,8 @@ public class Main {
 
         //parse adj
         for (int i = 0; i < M; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int a = readInt();
+            int b = readInt();
 
             adj[a].add(b);
             adj[b].add(a);
