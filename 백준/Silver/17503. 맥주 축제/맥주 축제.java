4a32c5d27a7d53d615800dad9113904c234f7552
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-03-07 21:03:51 +0900

    [Silver I] Title: 맥주 축제, Time: 952 ms, Memory: 79328 KB -BaekjoonHub

--- 백준/Silver/17503. 맥주 축제/맥주 축제.java
+++ 백준/Silver/17503. 맥주 축제/맥주 축제.java
@@ -3,12 +3,20 @@ import java.util.*;
 
 public class Main {
 
-    private static class Beer{
+    private static class Beer implements Comparable<Beer> {
         int prefer, level;
 
         public Beer(int prefer, int level) {
             this.prefer = prefer;
             this.level = level;
+        }
+
+        @Override
+        public int compareTo(Beer o) {
+            if (this.level == o.level){
+                return o.prefer - this.prefer;
+            }
+            return this.level - o.level;
         }
     }
 
@@ -19,12 +27,7 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int preferSum = 0, minLevel = 0;
-        PriorityQueue<Beer> levelPq = new PriorityQueue<>((o1, o2) -> {
-            if (o1.level == o2.level){
-                return o2.prefer - o1.prefer;
-            }
-            return o1.level - o2.level;
-        });
+        PriorityQueue<Beer> levelPq = new PriorityQueue<>();
         PriorityQueue<Beer> preferPq = new PriorityQueue<>(Comparator.comparingInt(b -> b.prefer));
 
         for (int i = 0; i < K; i++){
