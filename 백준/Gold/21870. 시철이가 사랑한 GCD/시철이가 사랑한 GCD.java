2d49f8b0f2b2783ea1068febaf5793cd93ecc305
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-03-13 19:55:06 +0900

    [Gold V] Title: 시철이가 사랑한 GCD, Time: 392 ms, Memory: 39192 KB -BaekjoonHub

---
+++ 백준/Gold/21870. 시철이가 사랑한 GCD/시철이가 사랑한 GCD.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int N, maxGcdSum;
+    private static int[] arr;
+
+    private static int gcd(int a, int b){
+        int big = Math.max(a, b), small = Math.min(a, b);
+
+        while (big % small > 0){
+            int tmp = small;
+            small = big % small;
+            big = tmp;
+        }
+        return small;
+    }
+
+    private static int gcdAll(int start, int end){
+        int res = arr[start];
+
+        for (int i = start + 1; i <= end; i++){
+            res = gcd(res, arr[i]);
+        }
+//        System.out.println(start+"~"+end+" "+res);
+        return res;
+    }
+
+    private static void getMaxGcd(int l, int r, int gcdSum){
+        if (l >= r){
+            maxGcdSum = Math.max(maxGcdSum, gcdSum + arr[l]);
+            return;
+        }
+
+        int m = (l + r - 1) / 2;
+
+        //오른쪽 반 선택
+        getMaxGcd(l, m, gcdSum + gcdAll(m + 1, r));
+        //왼쪽 반 선택
+        getMaxGcd(m + 1, r, gcdSum + gcdAll(l, m));
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N ;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        getMaxGcd(0, N - 1, 0);
+        System.out.println(maxGcdSum);
+    }
+
+}
