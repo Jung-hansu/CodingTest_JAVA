d603818cbf444a50c74ceeb4b1d0bd65c6e7507e
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-03-14 00:56:51 +0900

    [Gold V] Title: 시철이가 사랑한 GCD, Time: 368 ms, Memory: 38320 KB -BaekjoonHub

--- 백준/Gold/21870. 시철이가 사랑한 GCD/시철이가 사랑한 GCD.java
+++ 백준/Gold/21870. 시철이가 사랑한 GCD/시철이가 사랑한 GCD.java
@@ -3,21 +3,16 @@ import java.util.*;
 
 public class Main {
 
-    private static int N, maxGcdSum;
-    private static int[] arr;
-
     private static int gcd(int a, int b){
         int big = Math.max(a, b), small = Math.min(a, b);
 
         while (big % small > 0){
-            int tmp = small;
-            small = big % small;
-            big = tmp;
+            small = big % (big = small);
         }
         return small;
     }
 
-    private static int gcdAll(int start, int end){
+    private static int gcdAll(int[] arr, int start, int end){
         int res = arr[start];
 
         for (int i = start + 1; i < end; i++){
@@ -26,32 +21,31 @@ public class Main {
         return res;
     }
 
-    private static void getMaxGcd(int l, int r, int gcdSum){
-        if (l >= r - 1){
-            maxGcdSum = Math.max(maxGcdSum, gcdSum + arr[l]);
-            return;
+    // l inclusive, r exclusive
+    private static int getMaxGcd(int[] arr, int l, int r, int gcdSum){
+        if (l == r - 1){
+            return gcdSum + arr[l];
         }
 
         int m = (l + r) / 2;
-
-        //오른쪽 반 선택
-        getMaxGcd(l, m, gcdSum + gcdAll(m, r));
-        //왼쪽 반 선택
-        getMaxGcd(m, r, gcdSum + gcdAll(l, m));
+        return Math.max(
+                getMaxGcd(arr, m, r, gcdSum + gcdAll(arr, l, m)), 
+                getMaxGcd(arr, l, m, gcdSum + gcdAll(arr, m, r))
+        );
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
 
+        //parsing
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N ;i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        getMaxGcd(0, N, 0);
-        System.out.println(maxGcdSum);
+        System.out.println(getMaxGcd(arr, 0, N, 0));
     }
 
 }
