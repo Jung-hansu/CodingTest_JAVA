24a7c7df31a39f19039dbf5419e57057bc1058dc
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-03-16 01:53:30 +0900

    [Gold IV] Title: 세 수의 합, Time: 136 ms, Memory: 11996 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -13,20 +13,20 @@ public class Main {
         }
         Arrays.sort(nums);
 
-        // 세 원소의 합이 될 숫자를 먼저 정함
+        // 세 수의 합이 될 수를 먼저 정함
         for (int sumIdx = N - 1; sumIdx > 0; sumIdx--) {
-            // 세 원소중 두 원소를 고름 (가장 큰 원소 포함시 불가능)
-            for (int n1 = N - 2; n1 >= 0; n1--) {
-                for (int n2 = N - 2; n2 >= 0; n2--) {
-                    int n3 = nums[sumIdx] - (nums[n1] + nums[n2]);
+            // 세 수중 최대, 최소 수를 고름 (가장 큰 원소 포함시 불가능)
+            for (int n3 = N - 2; n3 >= 0; n3--) {
+                for (int n1 = 0; n1 <= n3; n1++) {
+                    int n2 = nums[sumIdx] - (nums[n1] + nums[n3]);
 
-                    //나머지 한 원소가 최솟값보다 작으면 불가능
-                    if (n3 < nums[0]) {
-                        continue;
+                    //나머지 한 수가 최솟값보다 작으면 불가능
+                    if (n2 < n1) {
+                        break;
                     }
 
-                    // 구간 이분탐색으로 나머지 원소 찾기 (0부터 n2-1까지 이분탐색)
-                    if (Arrays.binarySearch(nums, 0, n2 + 1, n3) >= 0) {
+                    // 구간 이분탐색으로 사이에 있는 나머지 수 찾기
+                    if (Arrays.binarySearch(nums, n1, n3 + 1, n2) >= 0) {
                         System.out.println(nums[sumIdx]);
                         return;
                     }
