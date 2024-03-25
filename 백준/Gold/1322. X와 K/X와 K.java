46dfaccfdd799311928d5a321636ae130f8f2d3f
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-03-25 22:52:33 +0900

    [Gold IV] Title: X와 K, Time: 76 ms, Memory: 11424 KB -BaekjoonHub

--- 백준/Gold/1322. X와 K/X와 K.java
+++ 백준/Gold/1322. X와 K/X와 K.java
@@ -1,12 +1,16 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
+    
+    private static int readInt() throws IOException{
+        int c, n = 0;
+        while ((c = System.in.read()) > 32)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int X = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken());
+        int X = readInt(), K = readInt();
         long res = 0;
 
         for (int i = 0; K > 0; i++){
