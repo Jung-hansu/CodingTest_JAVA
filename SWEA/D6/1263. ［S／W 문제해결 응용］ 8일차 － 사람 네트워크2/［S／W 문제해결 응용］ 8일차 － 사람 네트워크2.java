ab117a60ba5204a37f6760d8edfb0e7397e574d9
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-03-28 15:50:10 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 1,378 ms, Memory: 113,516 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -1,54 +1,59 @@
 import java.io.*;
-
+ 
 public class Solution {
+     
+    private static final int INF = 1_000_000_000;
+     
+    private static int getMinCC(int[][] adj, int N) {
+        int minCC = INF;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		final int INF = 10000;
-		int T = Integer.parseInt(br.readLine());
+        floyd(adj, N);
+        for (int i = 0; i < N; i++) {
+            int cc = 0;
+             
+            for (int j = 0; j < N; j++) {
+            	if (i != j) {
+            		cc += adj[i][j];
+            	}
+            }
+            minCC = Math.min(minCC, cc);
+        }
+        return minCC;
+    }
+     
+    private static void floyd(int[][] adj, int N) {
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    adj[i][j] = Math.min(adj[i][j], adj[i][k] + adj[k][j]);
+                }
+            }
+        }
+    }
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+ 
+        for (int tc = 1; tc <= T; tc++) {
+            String[] split = br.readLine().split(" ");
+            int N = Integer.parseInt(split[0]);
+            int[][] adj = new int[N][N];
+             
+            //generate adjacency matrix
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    adj[i][j] = Integer.parseInt(split[i * N + j + 1]);
+                    if (adj[i][j] == 0) {
+                        adj[i][j] = INF;
+                    }
+                }
+            }
 
-		for (int tc = 1; tc <= T; tc++) {
-			String[] split = br.readLine().split(" ");
-			int N = Integer.parseInt(split[0]);
-			int[][] adj = new int[N][N];
-			
-			//generate adjacency matrix
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					adj[i][j] = Integer.parseInt(split[i * N + j + 1]);
-					if (adj[i][j] == 0) {
-						adj[i][j] = INF;
-					}
-				}
-			}
-			
-			//floyd-warshall
-			for (int k = 0; k < N; k++) {
-				for (int i = 0; i < N; i++) {
-					for (int j = 0; j < N; j++) {
-						if (i != j) {
-							adj[i][j] = Math.min(adj[i][k] + adj[k][j], adj[i][j]);
-						}
-					}
-				}
-			}
-			
-			int minCC = INF;
-			for (int i = 0; i < N; i++) {
-				int cc = 0;
-				
-				for (int j = 0; j < N ;j++) {
-					if (i != j) {
-						cc += adj[i][j];
-					}
-				}
-				minCC = Math.min(minCC, cc);
-			}
-			
-			sb.append('#').append(tc).append(' ').append(minCC).append('\n');
-		}
-		System.out.print(sb);
-	}
-
+            sb.append('#').append(tc).append(' ').append(getMinCC(adj, N)).append('\n');
+        }
+        System.out.print(sb);
+    }
+ 
 }
