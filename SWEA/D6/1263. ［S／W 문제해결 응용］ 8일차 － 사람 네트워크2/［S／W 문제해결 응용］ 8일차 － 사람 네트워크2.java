27e2a8069fbc88cb6d4e3cbe56776b2ffb77dd54
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-03-28 15:12:46 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 3,029 ms, Memory: 107,888 KB -BaekjoonHub

---
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	
+	private static final int INF = 1_000_000_000;
+	
+	private static int floyd(int[][] adj, int N) {
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (i != j) {
+						adj[i][j] = Math.min(adj[i][j], adj[i][k] + adj[k][j]);
+					}
+				}
+			}
+		}
+		
+		int minCC = INF;
+		for (int[] row : adj) {
+			int cc = 0;
+			
+			for (int n : row) {
+				if (n < INF) {
+					cc += n;
+				}
+			}
+			minCC = Math.min(minCC, cc);
+		}
+		
+		return minCC;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int[][] adj = new int[N][N];
+			
+			//generate adjacency matrix
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					adj[i][j] = st.nextToken().equals("1") ? 1 : INF;
+					if (i == j) {
+						adj[i][j] = 0;
+					}
+				}
+			}
+			
+			sb.append('#').append(tc).append(' ').append(floyd(adj, N)).append('\n');
+		}
+		System.out.print(sb);
+	}
+
+}
