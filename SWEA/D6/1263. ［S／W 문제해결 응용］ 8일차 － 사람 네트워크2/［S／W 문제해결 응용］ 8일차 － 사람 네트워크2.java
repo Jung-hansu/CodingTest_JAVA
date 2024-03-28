e33b264f13b630b8aa205d88f90ed2fc833bc8fd
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-03-28 15:37:50 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 3,589 ms, Memory: 116,904 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -12,7 +12,9 @@ public class Solution {
 			int cc = 0;
 			
 			for (int j = 0; j < N ;j++) {
-				cc += adj[i][j];
+				if (i != j) {
+					cc += adj[i][j];
+				}
 			}
 			minCC = Math.min(minCC, cc);
 		}
@@ -45,14 +47,13 @@ public class Solution {
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
 					adj[i][j] = Integer.parseInt(split[i * N + j + 1]);
-					if (i != j && adj[i][j] == 0) {
+					if (adj[i][j] == 0) {
 						adj[i][j] = INF;
 					}
 				}
 			}
 			
-			int minCC = getMinCC(adj, N);
-			sb.append('#').append(tc).append(' ').append(minCC).append('\n');
+			sb.append('#').append(tc).append(' ').append(getMinCC(adj, N)).append('\n');
 		}
 		System.out.print(sb);
 	}
