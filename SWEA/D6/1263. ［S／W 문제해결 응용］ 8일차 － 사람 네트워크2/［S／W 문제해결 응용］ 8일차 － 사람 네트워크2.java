4d7ede1c22c8b0a46b90ffd6e134729ad1f929df
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-03-28 15:41:11 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 3,623 ms, Memory: 117,540 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -1,39 +1,11 @@
 import java.io.*;
 
 public class Solution {
-	
-	private static int getMinCC(int[][] adj, int N) {
-		int minCC = 1_000_000_000;
-		
-		floyd(adj, N);
-		for (int i = 0; i < N; i++) {
-			int cc = 0;
-			
-			for (int j = 0; j < N ;j++) {
-				if (i != j) {
-					cc += adj[i][j];
-				}
-			}
-			minCC = Math.min(minCC, cc);
-		}
-		return minCC;
-	}
-	
-	private static void floyd(int[][] adj, int N) {
-		for (int k = 0; k < N; k++) {
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (i != j) {
-						adj[i][j] = Math.min(adj[i][j], adj[i][k] + adj[k][j]);
-					}
-				}
-			}
-		}
-	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		final int INF = 1_000_000_000;
 		int T = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc <= T; tc++) {
@@ -46,12 +18,35 @@ public class Solution {
 				for (int j = 0; j < N; j++) {
 					adj[i][j] = Integer.parseInt(split[i * N + j + 1]);
 					if (adj[i][j] == 0) {
-						adj[i][j] = 1_000_000_000;
+						adj[i][j] = INF;
 					}
 				}
 			}
 			
-			sb.append('#').append(tc).append(' ').append(getMinCC(adj, N)).append('\n');
+			//floyd-warshall
+			for (int k = 0; k < N; k++) {
+				for (int i = 0; i < N; i++) {
+					for (int j = 0; j < N; j++) {
+						if (i != j) {
+							adj[i][j] = Math.min(adj[i][j], adj[i][k] + adj[k][j]);
+						}
+					}
+				}
+			}
+			
+			int minCC = INF;
+			for (int i = 0; i < N; i++) {
+				int cc = 0;
+				
+				for (int j = 0; j < N ;j++) {
+					if (i != j) {
+						cc += adj[i][j];
+					}
+				}
+				minCC = Math.min(minCC, cc);
+			}
+			
+			sb.append('#').append(tc).append(' ').append(minCC).append('\n');
 		}
 		System.out.print(sb);
 	}
