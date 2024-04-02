7c3877fd2f7452cc566eabe8ae3a15ab9be410e1
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-04-02 14:16:25 +0900

    [Bronze II] Title: 피보나치 수, Time: 76 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class Main {
+	
+	//{A(n-1), A(n)}
+	private static long[] arr = {0, 1};
+	
+	//arr => {A(2n-1), A(2n)}
+	private static void getDoubleFibo() {
+		long tmp1 = arr[0], tmp2 = arr[1];
+		
+		arr[0] = (tmp1 * tmp1 + tmp2 * tmp2);
+		arr[1] = (2 * tmp1 * tmp2 + tmp2 * tmp2);
+	}
+	
+	//arr => {A(n), A(n+1)}
+	private static void getNextFibo() {
+		long tmp1 = arr[0], tmp2 = arr[1];
+		
+		arr[0] = tmp2;
+		arr[1] = (tmp1 + tmp2);
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long N = Long.parseLong(br.readLine());
+		
+		if (N <= 1) {
+			System.out.println(N);
+			return;
+		}
+		
+		for (long b = Long.highestOneBit(N) >> 1; b > 0; b >>= 1) {
+			getDoubleFibo();
+			if ((N & b) > 0) {
+				getNextFibo();
+			}
+		}
+
+		System.out.println(arr[1]);
+	}
+	
+}
