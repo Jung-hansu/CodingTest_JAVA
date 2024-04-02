b886824762a2be2c5082b26a0349810a051c65d6
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-04-02 22:50:10 +0900

    [D4] Title: [Professional] 구간 합, Time: 108 ms, Memory: 24,380 KB -BaekjoonHub

---
+++ SWEA/D4/5604. ［Professional］ 구간 합/README.md
@@ -0,0 +1,15 @@
+# [D4] [Professional] 구간 합 - 5604 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWXGGNB6cnEDFAUo) 
+
+### 성능 요약
+
+메모리: 24,380 KB, 시간: 108 ms, 코드길이: 1,461 Bytes
+
+### 제출 일자
+
+2024-04-02 22:50
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/5604. ［Professional］ 구간 합/［Professional］ 구간 합.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    private static final int[] sigma = {0, 1, 3, 6, 10, 15, 21, 28, 36, 45};
+    private static final Map<Long, Long> cache = new HashMap<>();
+
+    private static boolean isNines(long N){
+        return N == 9 || N % 10 == 9 && isNines(N / 10);
+    }
+
+    private static long getDigit(long N){
+        return N > 9 ? getDigit(N / 10) * 10 : 1;
+    }
+
+    private static long S(long N){
+        if (N < 10){
+            return N > 0 ? sigma[(int)N] : 0;
+        }
+        if (isNines(N)){
+            return 45L * ((long)Math.log10(N + 1)) * getDigit(N);
+        }
+        if (cache.containsKey(N)){
+            return cache.get(N);
+        }
+
+        long v = getDigit(N);
+        long res = N / v * (N % v + 1) + S(N % v) + S(N - 1 - N % v);
+
+        cache.put(N, res);
+        return res;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long A = Long.parseLong(st.nextToken());
+            long B = Long.parseLong(st.nextToken());
+
+            sb.append('#').append(tc).append(' ').append(- S(A-1) + S(B)).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+}
