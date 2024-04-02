0055eaa9f94c6c44c6eb839fa32442fc304afcbd
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-04-02 14:10:31 +0900

    [Gold II] Title: 피보나치 수 3, Time: 80 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
@@ -1,33 +1,43 @@
 import java.io.*;
-import java.util.*;
-import java.math.BigInteger;
 
 public class Main {
-    private static long[] arr = new long[]{0, 1};
-    private static final int M = 1000000;
-    private static void get2nFib(){
-        long a = arr[0], b = arr[1];
-        arr[0] = (a*a%M + b*b%M) % M;
-        arr[1] = (2*a*b%M + b*b%M) % M;
-    }
-    private static void getNextFib(){
-        long a = arr[0], b = arr[1];
-        arr[0] = b;
-        arr[1] = (a+b) % M;
-    }
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BigInteger N = new BigInteger(br.readLine());
-        BitSet bs = new BitSet();
+	
+	private static final int M = 1_000_000;
+	private static long[] arr = {0, 1}; //{A(n-1), A(n)}
+	
+	//arr => {A(2n-1), A(2n)}
+	private static void getDoubleFibo() {
+		long tmp1 = arr[0], tmp2 = arr[1];
+		
+		arr[0] = (tmp1 * tmp1 % M + tmp2 * tmp2 % M) % M;
+		arr[1] = (2 * tmp1 * tmp2 % M + tmp2 * tmp2 % M) % M;
+	}
+	
+	//arr => {A(n), A(n+1)}
+	private static void getNextFibo() {
+		long tmp1 = arr[0], tmp2 = arr[1];
+		
+		arr[0] = tmp2;
+		arr[1] = (tmp1 + tmp2) % M;
+	}
 
-        for (int i = 0; !N.equals(BigInteger.ZERO); i++, N = N.shiftRight(1))
-            if (N.mod(BigInteger.TWO).equals(BigInteger.ONE))
-                bs.set(i);
-        int highestBit = bs.length()-1;
-        for (int i = highestBit; i >= 0; i--) {
-            if (i < highestBit && bs.get(i)) getNextFib();
-            if (i > 0) get2nFib();
-        }
-        System.out.println(arr[1]);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long N = Long.parseLong(br.readLine());
+		
+		if (N <= 1) {
+			System.out.println(N);
+			return;
+		}
+		
+		for (long b = Long.highestOneBit(N) >> 1; b > 0; b >>= 1) {
+			getDoubleFibo();
+			if ((N & b) > 0) {
+				getNextFibo();
+			}
+		}
+
+		System.out.println(arr[1]);
+	}
+	
 }
