8c48be81dc5fb3e42f2453408e25f8dd8dac3613
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-04-21 16:58:46 +0900

    [Gold I] Title: 수열과 쿼리 16, Time: 360 ms, Memory: 23832 KB -BaekjoonHub

--- 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.java
+++ 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 class SegmentTree{
 
@@ -11,7 +10,6 @@ class SegmentTree{
             this.val = val;
         }
 
-        //두 노드 중 작은 노드 반환
         public static Node min(Node n1, Node n2){
             if (n1 == null){
                 return n2;
@@ -26,17 +24,11 @@ class SegmentTree{
             }
             return n1.val < n2.val ? n1 : n2;
         }
-
-        @Override
-        public String toString() {
-            return idx + ":" + val;
-        }
     }
 
     private final Node[] tree;
     private final int leafLen, arrLen;
 
-    //리프노드가 arr인 완전이진트리 생성
     public SegmentTree(int[] arr){
         arrLen = arr.length;
         leafLen = Integer.highestOneBit(arrLen) << 1;
@@ -44,7 +36,6 @@ class SegmentTree{
         init(arr, 1, 1, leafLen);
     }
 
-    //루트노드(tree[1])가 최소 노드
     public int getMinIdx(int from, int to){
         return getMinIdx(1, from, to, 1, leafLen).idx;
     }
@@ -53,8 +44,6 @@ class SegmentTree{
         update(idx, newVal, 1, 1, leafLen);
     }
 
-    //재귀로 초기화
-    //리프노드는 arr, 비리프노드는 arr의 [start,end] 범위 내의 최소 노드를 나타냄
     private Node init(int[] arr, int node, int start, int end){
         if (start > arrLen){
             return null;
@@ -90,15 +79,12 @@ class SegmentTree{
         return Node.min(left, right);
     }
 
-    //재귀로 업데이트
-    //리프노드까지 내려가 업데이트한 뒤 거슬러 올라오며 최소 노드 업데이트
     private void update(int idx, int newVal, int node, int start, int end){
         if (start == end){
             tree[node].val = newVal;
             return;
         }
 
-        //수정할 인덱스 찾아가기
         if (idx <= (start + end) / 2) {
             update(idx, newVal, node * 2, start, (start + end) / 2);
         } else {
@@ -107,38 +93,34 @@ class SegmentTree{
         tree[node] = Node.min(tree[node * 2], tree[node * 2 + 1]);
     }
 
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 1; i < tree.length; i++){
-            sb.append(tree[i]).append(Integer.bitCount(i + 1) == 1 ? '\n' : ' ');
-        }
-        return sb.toString();
-    }
 }
 
 public class Main {
+    
+    private static int readInt() throws IOException{
+        int c, n = 0;
+        
+        while ((c = System.in.read()) > 32){
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(br.readLine());
+        int N = readInt();
 
         int[] arr = new int[N];
         for (int i = 0; i < N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = readInt();
         }
 
         SegmentTree segmentTree = new SegmentTree(arr);
+        int M = readInt();
         for (int i = 0; i < M; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+            int a = readInt();
+            int b = readInt();
+            int c = readInt();
 
             if (a == 1){
                 segmentTree.update(b, c);
