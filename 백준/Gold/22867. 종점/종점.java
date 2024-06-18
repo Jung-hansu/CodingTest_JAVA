cf0b570b3fe7bbe3534b25ae070e34d3874301db
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-06-18 18:27:39 +0900

    [Gold V] Title: 종점, Time: 1816 ms, Memory: 287724 KB -BaekjoonHub

---
+++ 백준/Gold/22867. 종점/종점.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int convertToMS(String timeFormat){
+        String[] time = timeFormat.split("[:.]");
+        int h = Integer.parseInt(time[0]);
+        int m = Integer.parseInt(time[1]);
+        int s = Integer.parseInt(time[2]);
+        int ms = Integer.parseInt(time[3]);
+        return h * 60 * 60 * 1000 + m * 60 * 1000 + s * 1000 + ms;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
+        int N = Integer.parseInt(br.readLine()), maxSize = 0;
+        int[][] buses = new int[N][2];
+
+        for (int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            buses[i][0] = convertToMS(st.nextToken());
+            buses[i][1] = convertToMS(st.nextToken());
+        }
+
+        Arrays.sort(buses, Comparator.comparingInt((int[] bus) -> bus[0]).thenComparingInt(bus -> bus[1]));
+        for (int i = 0; i < N; i++){
+            while (!pq.isEmpty() && pq.element()[1] <= buses[i][0]){
+                pq.remove();
+            }
+            pq.add(buses[i]);
+            maxSize = Math.max(maxSize, pq.size());
+        }
+
+        System.out.println(maxSize);
+    }
+
+}
