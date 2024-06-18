5a656ffec3cfdbc731d49d8c67e41dfd3a5c1d6b
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-06-18 18:48:33 +0900

    [Gold V] Title: 종점, Time: 672 ms, Memory: 34864 KB -BaekjoonHub

--- 백준/Gold/22867. 종점/종점.java
+++ 백준/Gold/22867. 종점/종점.java
@@ -6,7 +6,7 @@ public class Main {
     private static int readInt() throws IOException{
         int c, n = 0;
 
-        while ((c = System.in.read()) > 32){
+        while ((c = System.in.read()) > 32 && Character.isDigit(c)){
             n = (n << 3) + (n << 1) + (c & 15);
         }
         return n;
@@ -14,27 +14,21 @@ public class Main {
 
     private static int readTimeAsMs() throws IOException{
         final int[] weight = {60 * 60 * 1000, 60 * 1000, 1000, 1};
-        int c, n = 0, w = 0, ms = 0;
+        int idx = 0, ms = 0;
 
-        while((c = System.in.read()) > 32){
-            if (Character.isDigit(c)) {
-                n = (n << 3) + (n << 1) + (c & 15);
-                continue;
-            }
-            ms += n * weight[w++];
-            n = 0;
+        for(int i = 0; i < 4; i++){
+            ms += readInt() * weight[idx++];
         }
-        return ms + n;
+        return ms;
     }
 
     public static void main(String[] args) throws IOException {
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
         int N = readInt(), maxSize = 0;
-        int[][] buses = new int[N][2];
+        int[][] buses = new int[N][];
 
         for (int i = 0; i < N; i++){
-            buses[i][0] = readTimeAsMs();
-            buses[i][1] = readTimeAsMs();
+            buses[i] = new int[]{readTimeAsMs(), readTimeAsMs()};
         }
 
         Arrays.sort(buses, Comparator.comparingInt(bus -> bus[0]));
