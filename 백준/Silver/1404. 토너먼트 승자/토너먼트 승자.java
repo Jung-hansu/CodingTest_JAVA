66cf46b11beea63ce9c7eec7b1f80be74caea9cf
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-06-25 20:17:43 +0900

    [Silver II] Title: 토너먼트 승자, Time: 68 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Silver/1404. 토너먼트 승자/토너먼트 승자.java
+++ 백준/Silver/1404. 토너먼트 승자/토너먼트 승자.java
@@ -3,22 +3,22 @@ import java.util.*;
 
 public class Main {
 
-    private static double[][] probs;
+    private static int[][] probs;
 
     private static double get1stWinProb(int n){
-        return probs[n][n ^ 1];
+        return probs[n][n ^ 1] / 100.0;
     }
     private static double get2ndWinProb(int n){
         return get1stWinProb(n) *
                 (probs[n][n ^ 2] * get1stWinProb(n ^ 2)
-                + probs[n][n ^ 3] * get1stWinProb(n ^ 3));
+                + probs[n][n ^ 3] * get1stWinProb(n ^ 3)) / 100.0;
     }
     private static double getFinalWinProb(int n){
         return get2ndWinProb(n) *
                 (probs[n][n ^ 4] * get2ndWinProb(n ^ 4)
                 + probs[n][n ^ 5] * get2ndWinProb(n ^ 5)
                 + probs[n][n ^ 6] * get2ndWinProb(n ^ 6)
-                + probs[n][n ^ 7] * get2ndWinProb(n ^ 7));
+                + probs[n][n ^ 7] * get2ndWinProb(n ^ 7)) / 100.0;
     }
 
     public static void main(String[] args) throws IOException {
@@ -26,13 +26,13 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        probs = new double[8][8];
+        probs = new int[8][8];
         for (int i = 0; i < 8; i++){
             for (int j = i; j < 8; j++){
                 if (i != j) {
                     int prob = Integer.parseInt(st.nextToken());
-                    probs[i][j] = prob / 100.0;
-                    probs[j][i] = (100 - prob) / 100.0;
+                    probs[i][j] = prob;
+                    probs[j][i] = 100 - prob;
                 }
             }
         }
