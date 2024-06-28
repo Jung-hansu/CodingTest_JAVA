271dadf88f3b2a86198221023c600d88d9c3bce0
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-06-28 13:21:52 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 80 ms, Memory: 11448 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,8 +1,20 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        int N = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
-        for (int i = 1; i <= N; i++) System.out.println("*".repeat(i));
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int c, n = 0;
+        
+        while((c = System.in.read()) > 32){
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++){
+                sb.append("*");
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
     }
 }
