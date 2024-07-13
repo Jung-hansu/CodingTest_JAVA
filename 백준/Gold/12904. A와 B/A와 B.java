3d8d4ef0d0339472e2c254f6a5efdd72a8103680
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-07-14 01:26:36 +0900

    [Gold V] Title: A와 B, Time: 64 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.java
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -4,23 +4,37 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String S = br.readLine();
-        StringBuilder T = new StringBuilder(br.readLine());
+        String S = br.readLine(), T = br.readLine();
+        int l = 0, r = T.length() - 1;
         boolean revMode = false;
 
-        while (T.length() > S.length()){
-            char c = T.charAt(revMode ? 0 : T.length() - 1);
+        while (r - l + 1 > S.length()){
+            char c = T.charAt(revMode ? l : r);
 
-            T.deleteCharAt(revMode ? 0 : T.length() - 1);
+            if (revMode) l++;
+            else r--;
+
             if (c == 'B'){
                 revMode = !revMode;
             }
         }
 
-        if (revMode){
-            T.reverse();
+        if (revMode) {
+            for (int i = 0; i < S.length(); i++) {
+                if (S.charAt(i) != T.charAt(r - i)) {
+                    System.out.println(0);
+                    return;
+                }
+            }
+        } else {
+            for (int i = 0; i < S.length(); i++) {
+                if (S.charAt(i) != T.charAt(l + i)) {
+                    System.out.println(0);
+                    return;
+                }
+            }
         }
-        System.out.println(S.contentEquals(T) ? 1 : 0);
+        System.out.println(1);
     }
 
 }
