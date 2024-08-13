0af70806d720477cbfb1cef1c504160a7e63f143
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-08-14 00:08:10 +0900

    [Silver I] Title: 트리 나라 관광 가이드, Time: 244 ms, Memory: 19664 KB -BaekjoonHub

--- 백준/Silver/15805. 트리 나라 관광 가이드/트리 나라 관광 가이드.java
+++ 백준/Silver/15805. 트리 나라 관광 가이드/트리 나라 관광 가이드.java
@@ -2,24 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    
+    private static int readInt() throws IOException{
+        int c, n = 0;
+    
+        while((c = System.in.read()) > 32){
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
+        int N = readInt();
         int[] parent = new int[N + 1 >> 1];
         int[] arr = new int[N];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = readInt();
         }
 
         int prev = arr[0];
         parent[prev] = -1;
         for (int i = 1; i < N; i++){
             int cur = arr[i];
-            
+
             if (cur != parent[prev]){
                 parent[cur] = prev;
             }
