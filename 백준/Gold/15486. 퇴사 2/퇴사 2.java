5a82ba0fcff213c43a8189548dbd219ae7938b71
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-10-11 17:12:17 +0900

    [Gold V] Title: 퇴사 2, Time: 588 ms, Memory: 302456 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -6,19 +6,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[][] schedules = new int[N + 2][2];
         int[] dp = new int[N + 2];
 
         for (int i = 1; i <= N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            schedules[i][0] = Integer.parseInt(st.nextToken());
-            schedules[i][1] = Integer.parseInt(st.nextToken());
-        }
+            int term = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= N + 1; i++){
-            dp[i] = Math.max(dp[i], dp[i - 1]);
-            if (i + schedules[i][0] <= N + 1){
-                dp[i + schedules[i][0]] = Math.max(dp[i + schedules[i][0]], dp[i] + schedules[i][1]);
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+            if (i + term <= N + 1){
+                dp[i + term] = Math.max(dp[i + term], dp[i] + cost);
             }
         }
         System.out.println(dp[N + 1]);
