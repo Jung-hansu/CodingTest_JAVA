72318d27be5c9855819ceba2fc2a484d86ba21e0
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-10-21 17:22:47 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 696 ms, Memory: 137504 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
@@ -3,66 +3,36 @@ import java.util.*;
 
 public class Main {
 
-    private static class Pair implements Comparable<Pair>{
-        int val, idx;
-
-        public Pair(int val, int idx) {
-            this.val = val;
-            this.idx = idx;
-        }
-
-        @Override
-        public int compareTo(Pair p){
-            if (this.val == p.val){
-                this.idx = p.idx = -1;
-            }
-            return this.val - p.val;
-        }
-
-        public static boolean equals(Pair[] pairs1, Pair[] pairs2){
-            for (int i = 0; i < pairs1.length; i++){
-                if (pairs1[i].idx != pairs2[i].idx){
-                    return false;
-                }
-            }
-            return true;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        Pair[][] universes = new Pair[M][N];
+        int[][] universe = new int[M][N];
+        int[][] buf = new int[M][N];
 
         for (int i = 0; i < M; i++){
+
             st = new StringTokenizer(br.readLine());
-
             for (int j = 0; j < N; j++){
-                int planet = Integer.parseInt(st.nextToken());
-                universes[i][j] = new Pair(planet, j);
+                universe[i][j] = Integer.parseInt(st.nextToken());
             }
-            Arrays.sort(universes[i]);
+            for (int j = 0; j < N; j++){
+                buf[i][j] = Arrays.binarySearch(universe[i], universe[i][j]);
+            }
         }
-
-        BitSet check = new BitSet();
-        int pairs = 0;
+        
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < M; i++){
-            if (check.get(i)){
-                continue;
-            }
-            int cnt = 1;
-            check.set(i);
-            for (int j = i + 1; j < M; j++){
-                if (!check.get(j) && Pair.equals(universes[i], universes[j])){
-                    check.set(j);
-                    cnt++;
-                }
-            }
-            pairs += cnt * (cnt - 1) / 2;
+            int hash = Arrays.hashCode(buf[i]);
+            map.put(hash, map.getOrDefault(hash, 0) + 1);
         }
-
+        
+        int pairs = 0;
+        for (int num : map.values()){
+            pairs += num * (num - 1) / 2;
+        }
+        
         System.out.println(pairs);
     }
 
