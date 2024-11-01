5641bfc5816e212bfde33d70b67865d20dbe679e
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-11-02 00:02:47 +0900

    [Silver IV] Title: 피사노 주기, Time: 92 ms, Memory: 12380 KB -BaekjoonHub

--- 백준/Silver/9471. 피사노 주기/피사노 주기.java
+++ 백준/Silver/9471. 피사노 주기/피사노 주기.java
@@ -1,24 +1,17 @@
 import java.io.*;
 import java.util.*;
-
+// 0 1
 public class Main {
 
-    private static final int[] fibonacci = new int[5_000_000];
-    static {
-        fibonacci[1] = fibonacci[2] = 1;
-    }
-
     private static int k(int n){
-        if (n == 2){
-            return 3;
-        }
-
+        int[] arr = {1, 1};
         int i = 2;
-        while (fibonacci[i] % n != 1 || fibonacci[i - 1] % n != 0){
-            fibonacci[i + 1] = (fibonacci[i] + fibonacci[i - 1]) % n;
+
+        while (arr[i % 2] != 0 || arr[(i + 1) % 2] != 1){
+            arr[(i + 1) % 2] = (arr[0] + arr[1]) % n;
             i++;
         }
-        return i - 1;
+        return i;
     }
 
     public static void main(String[] args) throws IOException {
@@ -30,7 +23,6 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             st.nextToken();
             int M = Integer.parseInt(st.nextToken());
-
             sb.append(tc).append(" ").append(k(M)).append("\n");
         }
         System.out.print(sb);
