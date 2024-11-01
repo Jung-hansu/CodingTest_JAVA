13e9f8c26e44c5e4543d5e38dd8b44b8cfcb24c2
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-11-01 21:53:15 +0900

    [Silver IV] Title: 오리와 박수치는 춘배, Time: 100 ms, Memory: 11972 KB -BaekjoonHub

--- 백준/Silver/30404. 오리와 박수치는 춘배/오리와 박수치는 춘배.java
+++ 백준/Silver/30404. 오리와 박수치는 춘배/오리와 박수치는 춘배.java
@@ -2,17 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    
+    private static int readInt() throws IOException{
+        int c, n = 0;
+    
+        while((c = System.in.read()) > 32){
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int N = readInt();
+        int K = readInt();
 
         int clapCnt = 0, time = 0;
-        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int curDuck = Integer.parseInt(st.nextToken());
+            int curDuck = readInt();
 
             if (time < curDuck){
                 time = curDuck + K;
