6168fde5b55d956a01bc106888c764b0b0754eeb
hanjung <97068614+Jung-hansu@users.noreply.github.com>
2024-11-01 21:52:44 +0900

    [Silver IV] Title: 오리와 박수치는 춘배, Time: 216 ms, Memory: 25800 KB -BaekjoonHub

--- 백준/Silver/30404. 오리와 박수치는 춘배/오리와 박수치는 춘배.java
+++ 백준/Silver/30404. 오리와 박수치는 춘배/오리와 박수치는 춘배.java
@@ -3,35 +3,20 @@ import java.util.*;
 
 public class Main {
 
-    private static int getNextDuckIdx(int[] duck, int time){
-        int idx = Arrays.binarySearch(duck, time);
-
-        if (idx < 0){
-            idx = -idx - 1;
-        }
-        return idx;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[] duck = new int[N];
 
+        int clapCnt = 0, time = 0;
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            duck[i] = Integer.parseInt(st.nextToken());
-        }
+            int curDuck = Integer.parseInt(st.nextToken());
 
-        int time = duck[0], clapCnt = 0;
-        while(time <= duck[N - 1]){
-            time += K + 1;
-            clapCnt++;
-            int nextDuckIdx = getNextDuckIdx(duck, time);
-            
-            if (nextDuckIdx < N){
-                time = duck[nextDuckIdx];
+            if (time < curDuck){
+                time = curDuck + K;
+                clapCnt++;
             }
         }
         System.out.println(clapCnt);
